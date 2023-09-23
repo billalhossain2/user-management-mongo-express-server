745ac1ea141afae923b83b4ab7463ffe5f27f98d
billal-webdev <billal-webdev@gmail.com>
2023-09-23 21:05:59 +0600

    create, create, update, delete api added

--- index.js
+++ index.js
@@ -1,16 +1,95 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
+//middlewares
 app.use(cors());
+app.use(express.json())
 
-const port = process.env.port || 9000;
+const port = process.env.PORT || 9000;
 
 app.get("/", (req, res)=>{
-    res.send(`Server is listening on the port ${port}`)
+    res.send(`User management server is listening on the port ${port}`)
 })
 
+
+
+
+
+const uri = "mongodb+srv://billalwebdev:N59lkrJbz6KXg19C@cluster0.0ak1okw.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const database = client.db("usersDB");
+    const usersCollection = database.collection("users")
+
+    app.post("/user", async(req, res)=>{
+        const user = req.body;
+        const result = await usersCollection.insertOne(user)
+        res.send(result)
+    })
+
+    app.get("/users", async(req, res)=>{
+        const users = await usersCollection.find({}).toArray()
+        res.send(users)
+    })
+
+    app.delete("/user/:userId", async(req, res)=>{
+        const {userId} = req.params;
+        const result = await usersCollection.deleteOne({"_id":new ObjectId(userId)})
+        res.send(result)
+    })
+
+    app.put("/user/:userId", async(req, res)=>{
+        const {userId} = req.params;
+        const updateUser = req.body;
+        
+        const filter = {"_id":new ObjectId(userId)}
+        const options = {upsert:true}
+        const updateDoc = {
+            $set:updateUser
+        }
+
+        const result = await usersCollection.updateOne(filter, updateDoc, options)
+        res.send(result)
+    })
+    
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(port, ()=>{
-    console.log(`Server is running on the port ${port}`)
+    console.log(`User management server is running on the port ${port}`)
 })
 
